d0660756fe392bad0333a5bb2458d5577e95720c
Rajat Gupta <realrajatg@gmail.com>
2026-02-24 15:56:34 +0530

    Update Temprature-Monitoring-System_RajatGupta_202501100700114_B.py

--- Temprature-Monitoring-System_RajatGupta_202501100700114_B.py
+++ Temprature-Monitoring-System_RajatGupta_202501100700114_B.py
@@ -1,44 +1,52 @@
 # -----------------------------------------------------------
-# Grocery Billing System
+# Temperature Monitoring System
 # Name: Rajat Gupta
 # Roll No: 202501100700114
 # Branch: ECE-B
 # -----------------------------------------------------------
 
-#importing the uniform func from random module
+# Import required modules
 from random import uniform as temp
-import time 
+import time
 
-#Asking max and min limit of temprature from user
-min_temp,max_temp = map(int,input("Enter the min and max limits of temprature respectively: ").split())
+
+# Take minimum and maximum temperature limits from user
+min_temp, max_temp = map(
+    int,
+    input("Enter the min and max limits of temperature respectively: ").split()
+)
+
 time.sleep(0.5)
-
 print("-------------------------------")
 
+
+# Continuous monitoring loop
 while True:
-    print("Acquiring the temprature value...")
-    time.sleep(1)
-    temprature = round(temp(0,100),2)
-    print("Temprature Acquried...:",temprature)
+    print("Acquiring the temperature value...")
     time.sleep(1)
 
-    if temprature>max_temp:
-        for char in "Alert: Temperature is too high":
-            print(char,flush=False,end="")
-            time.sleep(0.05)
-        print("\n-------------------------------")
-    elif temprature>min_temp:
-        for char in "Temperature is within acceptable limit":
-            print(char,flush=False,end="")
-            time.sleep(0.05)
-        print("\n-------------------------------")
+    # Generate random temperature between 0 and 100
+    temperature = round(temp(0, 100), 2)
+
+    print("Temperature Acquired:", temperature)
+    time.sleep(1)
+
+    # Check temperature conditions
+    if temperature > max_temp:
+        message = "Alert: Temperature is too high"
+
+    elif temperature > min_temp:
+        message = "Temperature is within acceptable limit"
+
     else:
-        for char in "Alert: Temperature is too low":
-            print(char,flush=False,end="")
-            time.sleep(0.0520 )
-        print("\n-------------------------------")
-    
+        message = "Alert: Temperature is too low"
+
+    # Print message character by character (animation effect)
+    for char in message:
+        print(char, end="")
+        time.sleep(0.05)
+
+    print("\n-------------------------------")
+
+    # Wait before next reading
     time.sleep(2)
-    
-
-
